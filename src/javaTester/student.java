89e1a09a524d937060f708b85f903aaeac5c39e7
nghiatruong71 <nghiatruong71@gmail.com>
2022-09-25 21:57:05 +0700

    w

--- src/javaTester/student.java
+++ src/javaTester/student.java
@@ -5,8 +5,7 @@ import org.testng.Assert;
 public class student {
  
 	public void TC_02_ValidatePageTitle() {
-		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
+		
+		
 }
 }
